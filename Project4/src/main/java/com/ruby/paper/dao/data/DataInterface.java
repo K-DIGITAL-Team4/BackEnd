84bfac8ba2aea8df0ef707b128be314268379d2a
kyunggwan <4562sky@naver.com>
2023-01-31 17:17:59 +0900

    테이블 추가

---
+++ Project4/src/main/java/com/ruby/paper/dao/data/DataInterface.java
@@ -0,0 +1,13 @@
+package com.ruby.paper.dao.data;
+
+import java.util.Map;
+
+public interface DataInterface {
+
+	Map<String, Object> testInfo();
+
+	Map<String, Object> driveInfo();
+
+	Map<String, Object> vehicleInfo();
+
+}
