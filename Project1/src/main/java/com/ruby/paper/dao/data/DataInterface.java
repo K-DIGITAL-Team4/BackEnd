6503da4bde65c9c73ece39bcdfa22118446c4035
ckswns879 <ckswns879@naver.com>
2023-02-08 16:37:36 +0900

    Docs:DB명세서 업데이트

---
+++ Project1/src/main/java/com/ruby/paper/dao/data/DataInterface.java
@@ -0,0 +1,15 @@
+package com.ruby.paper.dao.data;
+
+import java.util.Map;
+
+public interface DataInterface {
+
+	Map<String, Object> simulInfo();
+
+	Map<String, Object> driveInfo();
+
+	Map<String, Object> vehicleInfo();
+
+	Map<String, Object> getSearch(String car_num);
+
+}
