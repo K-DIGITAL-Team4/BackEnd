a03d215c8fd8cb1697cd2641209fa29f5920057e
ckswns879 <ckswns879@naver.com>
2023-02-09 17:38:06 +0900

    Style: 불필요한 주석 삭제

--- Project-login/src/main/java/com/ruby/paper/controller/LoginController.java
+++ Project-login/src/main/java/com/ruby/paper/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.ruby.paper.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,27 +15,36 @@ import com.ruby.paper.service.LoginService;
 
 @RestController
 public class LoginController {
+	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+	public LoginController() {
+		log.info("LoginController() 생성자 호출됨.");
+	}
+	
 
 	@Autowired
 	private LoginService loginService;
 
-	@GetMapping("/read/{id}") // Read , 로그인
+	@GetMapping("/read/{id}") // 로그인
 	public MemberVO getUser(@PathVariable String id) {
+		log.info(String.format("MemberController - readMember('%s')가 호출됨.", id));
 		return loginService.getMember(id);
 	}
 
-	@PostMapping("/create") // Create , 회원가입
+	@PostMapping("/create") // 회원가입
 	public int addUser(MemberVO userVO) {
+		log.info("createmember 호출됨.");
 		return loginService.addMember(userVO);
 	}
 
-	@PutMapping("/update/{id}") // Update , 비밀번호변경
-	public int updateUser(@PathVariable String id, String pw) {
-		return loginService.updateMember(id, pw);
+	@PutMapping("/update/{id}") // 비밀번호변경
+	public int updateUser(@PathVariable String id, String pass) {
+		log.info(String.format("MemberController - updateMember('%s','%s')가 호출됨.", id, pass));
+		return loginService.updateMember(id, pass);
 	}
 
-	@DeleteMapping("/delete/{id}") // Delete , 아이디 삭제
+	@DeleteMapping("/delete/{id}") // 아이디 삭제
 	public int deleteUser(@PathVariable String id) {
+		log.info(String.format("MemberController - deleteMember('%s')가 호출됨.", id));
 		return loginService.deleteMember(id);
 	}
 
--- Project-login/src/main/java/com/ruby/paper/dao/LoginDao.java
+++ Project-login/src/main/java/com/ruby/paper/dao/LoginDao.java
@@ -24,22 +24,6 @@ public class LoginDao {
 	}
 
 	public int addMember(MemberVO memberVO) {
-		// 길고 복잡한 쿼리문 PreparedStatement 사용
-//		return jdbcTemplate.update(new PreparedStatementCreator() {
-//			@Override
-//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//				String query = "insert into member (id, pw) value (?, ?)";
-		// 파라미터로 전달받은 Connection을 이용해서 PreparedStatement 생성
-//				PreparedStatement pstmt = con.prepareStatement(query);
-		// 인덱스 파라미터 값 설정
-//				pstmt.setString(1, memberVO.getId());
-//				pstmt.setString(2, memberVO.getPw());
-		// 생성한 PreparedStatement 객체 리턴
-//				return pstmt;
-//			}
-//		});
-
-		// 짧고 단순한 쿼리문 String.format(문자열 형식 지정) 사용
 		String sql = String.format("insert into Member (id, pw) value ('%s', '%s')", memberVO.getId(), memberVO.getPass());
 		return jdbcTemplate.update(sql);
 	}
